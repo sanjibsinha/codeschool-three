ef880fb1cceccf835b1763dfd8c4f8dff5cbf33a
Sanjib <sanjib12sinha@gmail.com>
2021-07-04 04:22:52 +0530

    custom query finished

--- wp-content/themes/codeschool-three/header.php
+++ wp-content/themes/codeschool-three/header.php
@@ -32,7 +32,9 @@
               <li><a href="#">Programs</a></li>
               <li><a href="#">Events</a></li>
               <li><a href="#">Campuses</a></li>
-              <li><a href="<?php echo site_url( 'index.php/blog/' ); ?>">Blog</a></li>
+              <li
+              <?php if(get_post_type() == 'post') {echo 'class="current-menu-item"'; } ?>
+              ><a href="<?php echo site_url( 'index.php/blog/' ); ?>">Blog</a></li>
             </ul>
           </nav>
           <div class="site-header__util">
