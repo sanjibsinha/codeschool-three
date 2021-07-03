18b9173d3a5177db9859024f56b59df17c9bc3f9
Sanjib <sanjib12sinha@gmail.com>
2021-07-03 06:28:57 +0530

    adding blog page

--- wp-content/themes/codeschool-three/header.php
+++ wp-content/themes/codeschool-three/header.php
@@ -17,19 +17,23 @@
           <nav class="main-navigation">
           <?php
           
-          wp_nav_menu(array(
+          /*
+          if(wp_nav_menu(array(
             'theme_location' => 'header-menu'
-          ));
-          
+          )))
+          */
           
           ?>
-            <!-- <ul>
-              <li><a href="<?php echo site_url( '/index.php/about-us' ) ?>">About Us</a></li>
+            <ul>
+              <li <?php 
+              if(is_page('about-us') or wp_get_post_parent_id(0) == 10)
+              echo 'class="current-menu-item"'
+              ?>><a href="<?php echo site_url( '/index.php/about-us' ) ?>">About Us</a></li>
               <li><a href="#">Programs</a></li>
               <li><a href="#">Events</a></li>
               <li><a href="#">Campuses</a></li>
               <li><a href="#">Blog</a></li>
-            </ul> -->
+            </ul>
           </nav>
           <div class="site-header__util">
             <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
