7fe93eb711906be71fa6e4220e25f7a84c6e527b
Sanjib <sanjib12sinha@gmail.com>
2021-07-07 08:02:41 +0530

    updating layout

--- wp-content/themes/codeschool-three/front-page.php
+++ wp-content/themes/codeschool-three/front-page.php
@@ -65,20 +65,7 @@ get_header(); ?>
           </div>
           <?php  }         
           
-          ?>
-          <!-- <div class="event-summary">
-            <a class="event-summary__date event-summary__date--beige t-center" href="#">
-              <span class="event-summary__month">Feb</span>
-              <span class="event-summary__day">04</span>
-            </a>
-            <div class="event-summary__content">
-              <h5 class="event-summary__title headline headline--tiny"><a href="#"><?php // the_title(); ?></a></h5>
-              <p>Two of our professors have been in national news lately. <a href="#" class="nu gray">Read more</a></p>
-            </div>
-          </div> -->
-          
-
-          
+          ?>                           
 
           <p class="t-center no-margin"><a href="#" class="btn btn--yellow">View All Blog Posts</a></p>
         </div>
