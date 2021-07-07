92bbdbbb4251dbd9eaadd835a6a66838a6333468
Sanjib <sanjib12sinha@gmail.com>
2021-07-07 08:01:02 +0530

    disabling hero slider

--- wp-content/themes/codeschool-three/front-page.php
+++ wp-content/themes/codeschool-three/front-page.php
@@ -15,32 +15,19 @@ get_header(); ?>
     <div class="full-width-split group">
       <div class="full-width-split__one">
         <div class="full-width-split__inner">
-          <h2 class="headline headline--small-plus t-center">
-          <?php
-          echo "CodeSchool";
-          ?>
-          </h2>
-          <?php
-          $codeschoolPosts = new WP_Query(array(
-            'posts_per_page' => 2,
-            'category_name' => 'Codeschool',
-          ));
-          
-          while ($codeschoolPosts->have_posts()) {
-            $codeschoolPosts->the_post(); ?>
+          <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
 
           <div class="event-summary">
-            <a class="event-summary__date event-summary__date--beige t-center" href="#">
-              <span class="event-summary__month"><?php the_time( 'm' ) ?></span>
-              <span class="event-summary__day"><?php the_time( 'y' ) ?></span>
+            <a class="event-summary__date t-center" href="#">
+              <span class="event-summary__month">Mar</span>
+              <span class="event-summary__day">25</span>
             </a>
             <div class="event-summary__content">
-              <h5 class="event-summary__title headline headline--tiny"><a href="#"><?php the_title(); ?></a></h5>
-              <p><?php the_excerpt(); ?>. <a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
+              <h5 class="event-summary__title headline headline--tiny"><a href="#">Poetry in the 100</a></h5>
+              <p>Bring poems you&rsquo;ve wrote to the 100 building this Tuesday for an open mic and snacks. <a href="#" class="nu gray">Learn more</a></p>
             </div>
           </div>
-          <?php } ?>
-          <!-- <div class="event-summary">
+          <div class="event-summary">
             <a class="event-summary__date t-center" href="#">
               <span class="event-summary__month">Apr</span>
               <span class="event-summary__day">02</span>
@@ -49,22 +36,22 @@ get_header(); ?>
               <h5 class="event-summary__title headline headline--tiny"><a href="#">Quad Picnic Party</a></h5>
               <p>Live music, a taco truck and more can found in our third annual quad picnic day. <a href="#" class="nu gray">Learn more</a></p>
             </div>
-          </div> -->
+          </div>
+
           <p class="t-center no-margin"><a href="#" class="btn btn--blue">View All Events</a></p>
         </div>
       </div>
-
       <div class="full-width-split__two">
         <div class="full-width-split__inner">
-          <h2 class="headline headline--small-plus t-center"><?php echo "General"; ?></h2>
+          <h2 class="headline headline--small-plus t-center">From Our Blogs</h2>
           <?php
-          $generalPosts = new WP_Query(array(
+          $homePosts = new WP_Query(array(
             'posts_per_page' => 2,
             'category_name' => 'General',
           ));
           
-          while ($generalPosts->have_posts()) {
-            $generalPosts->the_post(); ?>
+          while ($homePosts->have_posts()) {
+            $homePosts->the_post(); ?>
             
           <div class="event-summary">
             <a class="event-summary__date event-summary__date--beige t-center" href="#">
@@ -89,19 +76,20 @@ get_header(); ?>
               <p>Two of our professors have been in national news lately. <a href="#" class="nu gray">Read more</a></p>
             </div>
           </div> -->
+          
+
+          
+
           <p class="t-center no-margin"><a href="#" class="btn btn--yellow">View All Blog Posts</a></p>
         </div>
       </div>
-      
     </div>
-
-    
 
     <!-- <div class="hero-slider">
       <div data-glide-el="track" class="glide__track">
         <div class="glide__slides">
           <div class="hero-slider__slide" style="background-image: url(<?php
-          // echo get_theme_file_uri('images/bus.jpg') ?>);">
+          echo get_theme_file_uri('images/bus.jpg') ?>);">
             <div class="hero-slider__interior container">
               <div class="hero-slider__overlay">
                 <h2 class="headline headline--medium t-center">Free Transportation</h2>
@@ -112,7 +100,7 @@ get_header(); ?>
           </div>
           <div class="hero-slider__slide" style="background-image: url(
               <?php
-          // echo get_theme_file_uri('images/apples.jpg') ?>
+          echo get_theme_file_uri('images/apples.jpg') ?>
               );">
             <div class="hero-slider__interior container">
               <div class="hero-slider__overlay">
@@ -124,7 +112,7 @@ get_header(); ?>
           </div>
           <div class="hero-slider__slide" style="background-image: url(
               <?php
-          // echo get_theme_file_uri('images/bread.jpg') ?>
+          echo get_theme_file_uri('images/bread.jpg') ?>
               );">
             <div class="hero-slider__interior container">
               <div class="hero-slider__overlay">
@@ -137,94 +125,8 @@ get_header(); ?>
         </div>
         <div class="slider__bullets glide__bullets" data-glide-el="controls[nav]"></div>
       </div>
-    </div> -->
-
-    <div class="full-width-split group">
-
-
-    <div class="full-width-split__two">
-        <div class="full-width-split__inner">
-          <h2 class="headline headline--small-plus t-center"><?php echo "General"; ?></h2>
-          <?php
-          $generalPosts = new WP_Query(array(
-            'posts_per_page' => 2,
-            'category_name' => 'General',
-          ));
-          
-          while ($generalPosts->have_posts()) {
-            $generalPosts->the_post(); ?>
-            
-          <div class="event-summary">
-            <a class="event-summary__date event-summary__date--beige t-center" href="#">
-              <span class="event-summary__month"><?php the_time( 'm' ) ?></span>
-              <span class="event-summary__day"><?php the_time( 'y' ) ?></span>
-            </a>
-            <div class="event-summary__content">
-              <h5 class="event-summary__title headline headline--tiny"><a href="#"><?php the_title(); ?></a></h5>
-              <p><?php the_excerpt(); ?>. <a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
-            </div>
-          </div>
-          <?php  }         
-          
-          ?>
-          <!-- <div class="event-summary">
-            <a class="event-summary__date event-summary__date--beige t-center" href="#">
-              <span class="event-summary__month">Feb</span>
-              <span class="event-summary__day">04</span>
-            </a>
-            <div class="event-summary__content">
-              <h5 class="event-summary__title headline headline--tiny"><a href="#"><?php // the_title(); ?></a></h5>
-              <p>Two of our professors have been in national news lately. <a href="#" class="nu gray">Read more</a></p>
-            </div>
-          </div> -->
-          <p class="t-center no-margin"><a href="#" class="btn btn--yellow">View All Blog Posts</a></p>
-        </div>
-      </div>
-
-      <div class="full-width-split__one">
-        <div class="full-width-split__inner">
-          <h2 class="headline headline--small-plus t-center">
-          <?php
-          echo "CodeSchool";
-          ?>
-          </h2>
-          <?php
-          $codeschoolPosts = new WP_Query(array(
-            'posts_per_page' => 2,
-            'category_name' => 'Codeschool',
-          ));
-          
-          while ($codeschoolPosts->have_posts()) {
-            $codeschoolPosts->the_post(); ?>
-
-          <div class="event-summary">
-            <a class="event-summary__date event-summary__date--beige t-center" href="#">
-              <span class="event-summary__month"><?php the_time( 'm' ) ?></span>
-              <span class="event-summary__day"><?php the_time( 'y' ) ?></span>
-            </a>
-            <div class="event-summary__content">
-              <h5 class="event-summary__title headline headline--tiny"><a href="#"><?php the_title(); ?></a></h5>
-              <p><?php the_excerpt(); ?>. <a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
-            </div>
-          </div>
-          <?php } ?>
-          <!-- <div class="event-summary">
-            <a class="event-summary__date t-center" href="#">
-              <span class="event-summary__month">Apr</span>
-              <span class="event-summary__day">02</span>
-            </a>
-            <div class="event-summary__content">
-              <h5 class="event-summary__title headline headline--tiny"><a href="#">Quad Picnic Party</a></h5>
-              <p>Live music, a taco truck and more can found in our third annual quad picnic day. <a href="#" class="nu gray">Learn more</a></p>
-            </div>
-          </div> -->
-          <p class="t-center no-margin"><a href="#" class="btn btn--blue">View All Events</a></p>
-        </div>
-      </div>
-
-      
-      
     </div>
+ -->
 
 <?php
 get_footer();
